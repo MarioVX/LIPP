4d3348a9973c92c4e6139650ee74021bfec8eb28
MarioVX <Mario.Kantz@gmail.com>
2024-08-23 17:05:45 +0200

    prepped last experiment, running on best settings on the RWC instances

---
+++ exp3.py
@@ -0,0 +1,20 @@
+import solverm
+import csv
+import os
+from datetime import datetime
+
+settings = {"iso":0, "sup":0, "ub":"ds", "dec":0, "bbt":0, "bbt_with_ub":"ds", "bbt_without_ub":"ds"}
+graph_dir_name = 'graphs/lip_crn'
+
+with open('experiment_' + datetime.now().strftime('%H%M%S') + '.csv', 'w', newline='') as resultfile:
+    writer = csv.writer(resultfile)
+    write.writerow(['name','success', 'prio/length', 'searchnodes', 'time'])
+    for graphfile in os.listdir(graph_dir_name):
+        g = nx.read_graphml(graph_dir_name + "/" + str(graphfile))
+        res = solverm.solve(g, settings)
+        line = [str(graphfile),]
+        if not res[0]:
+            line += [0, res[2], res[1], 1200.0]
+        else:
+            line += [1, len(res[0][0]), res[1], res[2]]
+        writer.writerow(line)
